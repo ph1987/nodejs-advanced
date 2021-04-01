952851a26563ead27c90775a4a7fb681a4d443f0
ph1987 <ph1987@gmail.com>
2021-04-01 09:01:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const crypto = require('crypto');  
+const app = express();
+
+app.get('/', (req,res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hi there');
+    });
+});
+
+app.get('/fast', (req,res) => {
+    res.send('This was fast!');
+});
+
+app.listen(3001);
+
+
